9ab9a080951e5f949e1a9e5ed170e5fa5c2b30a8
fukugit <fukuma1234@gmail.com>
2020-11-07 12:10:51 +0900

    デザインを色々変更

--- js/main.js
+++ js/main.js
@@ -5,9 +5,22 @@
 hljs.initHighlightingOnLoad();
 
 {
-  const box1 = document.getElementById("box1");
-  box1.addEventListener("click", () => {
-    // toggleとは、エレメントにクラスがなければ付ける。あれば外す。という処理です。
-    box1.classList.toggle("box1-animation-add");
+  // const box1 = document.getElementById("box1");
+  // box1.addEventListener("click", () => {
+  //   // toggleとは、エレメントにクラスがなければ付ける。あれば外す。という処理です。
+  //   box1.classList.toggle("box1-animation-add");
+  // });
+
+  const box1 = document.getElementById('box1');
+  const overlay = document.querySelector('.overlay');
+  const close = document.getElementById('close');
+
+  box1.addEventListener('click', () => {
+    overlay.classList.add('show');
+    box1.classList.add('hide');
   });
+  close.addEventListener('click', () => {
+    overlay.classList.remove('show');
+    box1.classList.remove('hide');
+  })
 }
