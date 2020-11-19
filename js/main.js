d44acecf7139b0c4b6f80385db991a954e759139
fukugit <fukuma1234@gmail.com>
2020-11-19 23:15:46 +0900

    新しいアニメーションを追加

--- js/main.js
+++ js/main.js
@@ -10,6 +10,11 @@ hljs.initHighlightingOnLoad();
   const overlay1 = document.getElementById('overlay1');
   const close1 = document.getElementById('close1');
   const click1 = document.getElementById("box1-main");
+  
+    const box2 = document.getElementById('box2');
+    const overlay2 = document.getElementById('overlay2');
+    const close2 = document.getElementById('close2');
+    const click2 = document.getElementById('box2-main');
 
   box1.addEventListener('click', () => {
     overlay1.classList.add('show');
@@ -23,10 +28,6 @@ hljs.initHighlightingOnLoad();
     click1.classList.toggle("box1-transition-animation");
   });
 
-  const box2 = document.getElementById('box2');
-  const overlay2 = document.getElementById('overlay2')
-  const close2 = document.getElementById('close2');
-
   box2.addEventListener('click', () => {
     overlay2.classList.add('show');
     box2.classList.add('hide');
@@ -34,5 +35,8 @@ hljs.initHighlightingOnLoad();
   close2.addEventListener('click', () => {
     overlay2.classList.remove('show');
     box2.classList.remove('hide');
+  });
+  click2.addEventListener("click", () => {
+    click2.classList.toggle("box2-transition-animation");
   });
 }
