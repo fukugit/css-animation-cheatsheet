7fcabe2bfa304302e2312d6262c8323f26a7c932
fukugit <fukuma1234@gmail.com>
2020-11-21 18:24:33 +0900

    HTMLタグの構造をリファクタ

--- js/main.js
+++ js/main.js
@@ -9,12 +9,10 @@ hljs.initHighlightingOnLoad();
   const box1 = document.getElementById('box1');
   const overlay1 = document.getElementById('overlay1');
   const close1 = document.getElementById('close1');
-  const click1 = document.getElementById('box1-main-id');
-  const clickhere1 = document.getElementById('click-here1');
-  const clickhere1anime = document.getElementById('click-here1-anime');
+  const boxmainid1 = document.getElementById('box1-main-id');
+  const buttonanime1 = document.getElementById('button-anime-1');
+  const buttontran1 = document.getElementById('button-transition-1');
   const box1mainanime = document.getElementById('box1-main-anime');
-  
-  
   
   box1.addEventListener('click', () => {
     overlay1.classList.add('show');
@@ -24,25 +22,30 @@ hljs.initHighlightingOnLoad();
     overlay1.classList.remove('show');
     box1.classList.remove('hide');
   });
-  click1.addEventListener("click", () => {
-    click1.classList.toggle("box1-transition-animation");
+  buttonanime1.addEventListener("click", () => {
+    box1mainanime.classList.toggle("box1-main-animate");
+    const label = buttonanime1.textContent;
+    if (label === 'start') {
+      document.getElementById('button-anime-1').textContent = 'stop';
+    } else if (label === 'stop') {
+      document.getElementById('button-anime-1').textContent = 'start';
+    }
   });
-  clickhere1.addEventListener("click", () => {
-    click1.classList.toggle("box1-transition-animation");
+  boxmainid1.addEventListener("click", () => {
+    boxmainid1.classList.toggle("box1-main-transition-additional");
   });
-  clickhere1anime.addEventListener("click", () => {
-    box1mainanime.classList.toggle("box1");
-  })
-
+  buttontran1.addEventListener("click", () => {
+    boxmainid1.classList.toggle("box1-main-transition-additional");
+  });
 
   const box2 = document.getElementById('box2');
   const overlay2 = document.getElementById('overlay2');
   const close2 = document.getElementById('close2');
-  const click2 = document.getElementById('box2-main-id');
-  const clickhere2 = document.getElementById('click-here2');
-  const clickhere2anime = document.getElementById('click-here2-anime');
+  const boxmainid2 = document.getElementById('box2-main-id');
+  const buttonanime2 = document.getElementById('button-anime-2');
+  const buttontran2 = document.getElementById('button-transition-2');
   const box2mainanime = document.getElementById('box2-main-anime');
-
+  
   box2.addEventListener('click', () => {
     overlay2.classList.add('show');
     box2.classList.add('hide');
@@ -51,13 +54,20 @@ hljs.initHighlightingOnLoad();
     overlay2.classList.remove('show');
     box2.classList.remove('hide');
   });
-  click2.addEventListener("click", () => {
-    click2.classList.toggle("box2-transition-animation");
+  buttonanime2.addEventListener("click", () => {
+    box2mainanime.classList.toggle("box2-main-animate");
+    const label = buttonanime2.textContent;
+    if (label === 'start') {
+      document.getElementById('button-anime-2').textContent = 'stop';
+    } else if (label === 'stop') {
+      document.getElementById('button-anime-2').textContent = 'start';
+    }
   });
-  clickhere2.addEventListener("click", () => {
-    click2.classList.toggle("box2-transition-animation");
+  boxmainid2.addEventListener("click", () => {
+    boxmainid2.classList.toggle("box2-main-transition-additional");
   });
-  clickhere2anime.addEventListener("click", () => {
-    box2mainanime.classList.toggle("box2");
-  })
+  buttontran2.addEventListener("click", () => {
+    boxmainid2.classList.toggle("box2-main-transition-additional");
+  });
+
 }
