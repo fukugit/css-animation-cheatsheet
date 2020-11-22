a88bb5fef89ffcd241bd0d00c30da0eecdcf0ebb
fukugit <fukugit@users.noreply.github.com>
2020-11-22 13:55:59 +0000

    deploy: 89b440930ecfe0deb8de5664c22cc06aade9adfd

--- js/main.js
+++ js/main.js
@@ -70,4 +70,36 @@ hljs.initHighlightingOnLoad();
     boxmainid2.classList.toggle("box2-main-transition-additional");
   });
 
+
+  const box3 = document.getElementById('box3');
+  const overlay3 = document.getElementById('overlay3');
+  const close3 = document.getElementById('close3');
+  const boxmainid3 = document.getElementById('box3-main-id');
+  const buttonanime3 = document.getElementById('button-anime-3');
+  const buttontran3 = document.getElementById('button-transition-3');
+  const box3mainanime = document.getElementById('box3-main-anime');
+  
+  box3.addEventListener('click', () => {
+    overlay3.classList.add('show');
+    box3.classList.add('hide');
+  });
+  close3.addEventListener('click', () => {
+    overlay3.classList.remove('show');
+    box3.classList.remove('hide');
+  });
+  buttonanime3.addEventListener("click", () => {
+    box3mainanime.classList.toggle("box3-main-animate");
+    const label = buttonanime3.textContent;
+    if (label === 'start') {
+      document.getElementById('button-anime-3').textContent = 'stop';
+    } else if (label === 'stop') {
+      document.getElementById('button-anime-3').textContent = 'start';
+    }
+  });
+  boxmainid3.addEventListener("click", () => {
+    boxmainid3.classList.toggle("box3-main-transition-additional");
+  });
+  buttontran3.addEventListener("click", () => {
+    boxmainid3.classList.toggle("box3-main-transition-additional");
+  });
 }
